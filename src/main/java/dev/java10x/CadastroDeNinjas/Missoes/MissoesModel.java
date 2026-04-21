675955d036c145cef68bb79df64b75ec339141e7
Adenilson <adenilsonsilvanazare@gmail.com>
2026-04-20 22:26:18 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -3,11 +3,18 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 import dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service.NinjaModel;
 import jakarta.persistence.*;
 import jdk.jfr.Enabled;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Enabled
 @Table (name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class MissoesModel {
 
     @Id
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -2,13 +2,19 @@ package dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service;
 
 import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-
 
 // Entity ele transforma uma classe em uma entidade do banco de dados
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class NinjaModel {
 
    @Id
