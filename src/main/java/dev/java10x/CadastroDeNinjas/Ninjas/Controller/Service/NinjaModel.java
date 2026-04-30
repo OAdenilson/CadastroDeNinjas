f0285c054ccf6401f0901acd690ff5ab56bee1bf
Adenilson <adenilsonsilvanazare@gmail.com>
2026-04-30 08:47:22 -0300

    Refactor: Inclui a annotation ToString nos models de Ninja e Missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -5,23 +5,39 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "tb_cadastro")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@ToString(exclude = "missoes")
+
 public class NinjaModel {
    // seus campos aqui, ex:
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+   @Column (name = "id")
    private Long id;
+
+   @Column (name = "nome")
    private String nome;
+
+   @Column ( name = "email")
    private String email;
+
+   @Column ( name = "imgUrl")
    private String imgUrl;
+
+   @Column ( name = "idade")
    private int idade;
+
+   @Column ( name = "rank")
    private String rank;
 
    @ManyToOne
+   @JoinColumn ( name = "missoes_id")
    private MissoesModel missoes;
 }
